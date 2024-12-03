2f0a2fc9465e937a745cf753f46aa64f5bbb7d57
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-03 12:28:34 -0800

    Upgrade modules

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -57,24 +57,14 @@ function _async_to_generator(fn) {
         });
     };
 }
+function _call_super(_this, derived, args) {
+    derived = _get_prototype_of(derived);
+    return _possible_constructor_return(_this, _is_native_reflect_construct() ? Reflect.construct(derived, args || [], _get_prototype_of(_this).constructor) : derived.apply(_this, args));
+}
 function _class_call_check(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
         throw new TypeError("Cannot call a class as a function");
     }
-}
-function _defineProperties(target, props) {
-    for(var i = 0; i < props.length; i++){
-        var descriptor = props[i];
-        descriptor.enumerable = descriptor.enumerable || false;
-        descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
-        Object.defineProperty(target, descriptor.key, descriptor);
-    }
-}
-function _create_class(Constructor, protoProps, staticProps) {
-    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) _defineProperties(Constructor, staticProps);
-    return Constructor;
 }
 function _get_prototype_of(o) {
     _get_prototype_of = Object.setPrototypeOf ? Object.getPrototypeOf : function getPrototypeOf(o) {
@@ -118,28 +108,12 @@ function _type_of(obj) {
     return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj;
 }
 function _is_native_reflect_construct() {
-    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
-    if (Reflect.construct.sham) return false;
-    if (typeof Proxy === "function") return true;
     try {
-        Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function() {}));
-        return true;
-    } catch (e) {
-        return false;
-    }
-}
-function _create_super(Derived) {
-    var hasNativeReflectConstruct = _is_native_reflect_construct();
-    return function _createSuperInternal() {
-        var Super = _get_prototype_of(Derived), result;
-        if (hasNativeReflectConstruct) {
-            var NewTarget = _get_prototype_of(this).constructor;
-            result = Reflect.construct(Super, arguments, NewTarget);
-        } else {
-            result = Super.apply(this, arguments);
-        }
-        return _possible_constructor_return(this, result);
-    };
+        var result = !Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function() {}));
+    } catch (_) {}
+    return (_is_native_reflect_construct = function() {
+        return !!result;
+    })();
 }
 function _ts_generator(thisArg, body) {
     var f, y, t, g, _ = {
@@ -253,114 +227,103 @@ function toJSON(results) {
 var Suite = /*#__PURE__*/ function(EventEmitter) {
     "use strict";
     _inherits(Suite, EventEmitter);
-    var _super = _create_super(Suite);
     function Suite(name, type) {
         _class_call_check(this, Suite);
         var _this;
-        _this = _super.call(this);
+        _this = _call_super(this, Suite);
         _this.name = name;
-        if (!type) throw new Error("Suite needs a test type");
+        if (!type) throw new Error('Suite needs a test type');
         _this.type = type;
         _this.Test = TESTS[_this.type];
         if (!_this.Test) throw new Error("Suite test type not recognized ".concat(type));
         _this.tests = [];
         return _this;
     }
-    _create_class(Suite, [
-        {
-            key: "add",
-            value: function add(name, fn) {
-                this.tests.push(new this.Test(name, fn));
-            }
-        },
-        {
-            key: "run",
-            value: function run(options) {
-                var _this = this;
-                return _async_to_generator(function() {
-                    var results, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, test, result, key, err;
-                    return _ts_generator(this, function(_state) {
-                        switch(_state.label){
-                            case 0:
-                                if (!options.time) throw new Error("Missing time option");
-                                results = {};
-                                _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
-                                _state.label = 1;
-                            case 1:
-                                _state.trys.push([
-                                    1,
-                                    6,
-                                    7,
-                                    8
-                                ]);
-                                _iterator = _this.tests[Symbol.iterator]();
-                                _state.label = 2;
-                            case 2:
-                                if (!!(_iteratorNormalCompletion = (_step = _iterator.next()).done)) return [
-                                    3,
-                                    5
-                                ];
-                                test = _step.value;
-                                return [
-                                    4,
-                                    test.run(options)
-                                ];
-                            case 3:
-                                result = _state.sent();
-                                for(var key in result){
-                                    if (!results[key] || _this.Test.metric(results[key].stats) < _this.Test.metric(result[key].stats)) results[key] = result[key];
-                                }
-                                _this.emit("cycle", toJSON(result));
-                                _state.label = 4;
-                            case 4:
-                                _iteratorNormalCompletion = true;
-                                return [
-                                    3,
-                                    2
-                                ];
-                            case 5:
-                                return [
-                                    3,
-                                    8
-                                ];
-                            case 6:
-                                err = _state.sent();
-                                _didIteratorError = true;
-                                _iteratorError = err;
-                                return [
-                                    3,
-                                    8
-                                ];
-                            case 7:
-                                try {
-                                    if (!_iteratorNormalCompletion && _iterator.return != null) {
-                                        _iterator.return();
-                                    }
-                                } finally{
-                                    if (_didIteratorError) {
-                                        throw _iteratorError;
-                                    }
-                                }
-                                return [
-                                    7
-                                ];
-                            case 8:
-                                _this.emit("complete", toJSON(results));
-                                return [
-                                    2
-                                ];
+    var _proto = Suite.prototype;
+    _proto.add = function add(name, fn) {
+        this.tests.push(new this.Test(name, fn));
+    };
+    _proto.run = function run(options) {
+        var _this = this;
+        return _async_to_generator(function() {
+            var results, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, test, result, key, err;
+            return _ts_generator(this, function(_state) {
+                switch(_state.label){
+                    case 0:
+                        if (!options.time) throw new Error('Missing time option');
+                        results = {};
+                        _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
+                        _state.label = 1;
+                    case 1:
+                        _state.trys.push([
+                            1,
+                            6,
+                            7,
+                            8
+                        ]);
+                        _iterator = _this.tests[Symbol.iterator]();
+                        _state.label = 2;
+                    case 2:
+                        if (!!(_iteratorNormalCompletion = (_step = _iterator.next()).done)) return [
+                            3,
+                            5
+                        ];
+                        test = _step.value;
+                        return [
+                            4,
+                            test.run(options)
+                        ];
+                    case 3:
+                        result = _state.sent();
+                        for(var key in result){
+                            if (!results[key] || _this.Test.metric(results[key].stats) < _this.Test.metric(result[key].stats)) results[key] = result[key];
                         }
-                    });
-                })();
-            }
-        },
-        {
-            key: "formatStats",
-            value: function formatStats(result) {
-                return this.Test.formatStats(result);
-            }
-        }
-    ]);
+                        _this.emit('cycle', toJSON(result));
+                        _state.label = 4;
+                    case 4:
+                        _iteratorNormalCompletion = true;
+                        return [
+                            3,
+                            2
+                        ];
+                    case 5:
+                        return [
+                            3,
+                            8
+                        ];
+                    case 6:
+                        err = _state.sent();
+                        _didIteratorError = true;
+                        _iteratorError = err;
+                        return [
+                            3,
+                            8
+                        ];
+                    case 7:
+                        try {
+                            if (!_iteratorNormalCompletion && _iterator.return != null) {
+                                _iterator.return();
+                            }
+                        } finally{
+                            if (_didIteratorError) {
+                                throw _iteratorError;
+                            }
+                        }
+                        return [
+                            7
+                        ];
+                    case 8:
+                        _this.emit('complete', toJSON(results));
+                        return [
+                            2
+                        ];
+                }
+            });
+        })();
+    };
+    _proto.formatStats = function formatStats(result) {
+        return this.Test.formatStats(result);
+    };
     return Suite;
 }(_eventemitter3.default);
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
